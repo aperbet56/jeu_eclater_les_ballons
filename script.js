07ddbf788a07da053dee311771f83fe9c2bc1098
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-09-09 19:12:17 +0200

    ajout de la fonction getCurrentYear

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const gallery = document.querySelector(".balloon__gallery");
 const message = document.querySelector(".yay-no-balloons");
 const replayBtn = document.querySelector(".replay__btn");
+const footerCopyrightYear = document.querySelector(".footer__text__year");
 
 // Création de la variable pop réglé de base à 0
 let popped = 0;
@@ -34,3 +35,19 @@ document.addEventListener("click", (e) => {
     checkAllPopped();
   }
 });
+
+// Déclaration de la fonction getCurrentYear qui va permettre l'affichage dynamique de l'année
+const getCurrentYear = () => {
+  // Récupération de la date actuelle stockée dans la constante date
+  const date = new Date();
+  //console.log(date);
+
+  // Récupération de l'année stockée dans la constante year
+  const year = date.getFullYear();
+  //console.log(year);
+
+  // Affichage dynamique de l'année en cours
+  footerCopyrightYear.textContent = `${year}`;
+};
+// Appel de la fonction getCurrentYear()
+getCurrentYear();
